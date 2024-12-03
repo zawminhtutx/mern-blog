45fc50835ab33e75aa474b21cead377744a254f3
Zaw Min Htut <zawminhtutx@gmail.com>
2024-12-03 16:29:05 +0630

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 function App() {
-  
-
   return (
-    <>
-      <div>Hello React
-              </div>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route  path="/sign-up" element={<SignUp/>}/>
+        <Route path="/dashboard" element ={<Dashboard/>}/>
+        <Route path="/projects"  element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
