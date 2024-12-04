17030369bd52be0e858f950455e0358c6c1ea489
Zaw Min Htut <zawminhtutx@gmail.com>
2024-12-04 15:05:02 +0630

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req,res)=>{
+    res.json({message:'controller api is working'});
+}
---
+++ api/routes/user.route.js
@@ -0,0 +1,9 @@
+import exp from 'constants';
+import express from 'express';
+import { test } from '../controllers/user.controller.js';
+
+const router =  express.Router();
+
+router.get('/test',test);
+
+export default router;
