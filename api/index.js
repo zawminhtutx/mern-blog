bc2ccf707ab4514445d7dd02275b423310b7a6db
Zaw Min Htut <zawminhtutx@gmail.com>
2024-12-04 14:24:47 +0630

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log(error); 
+})
 const app = express();
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
