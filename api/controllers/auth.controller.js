b99ddc1c60fb0bf5456c6994301e27fe3dd53a06
Zaw Min Htut <zawminhtutx@gmail.com>
2024-12-06 14:12:11 +0630

    create middleware and error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,29 +1,30 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
     !email ||
     !password ||
-    !username.trim() ||
-    !email.trim() ||
-    !password.trim()
+    username.trim() === "" ||
+    email.trim() === "" ||
+    password.trim() === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  try {
-    const newUser = new User({ 
-        username, 
-        email, 
-        password: hashedPassword,
-    });
-    await newUser.save();
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword
+  });
 
+  try {
+    await newUser.save();
     res.status(201).json({ message: "User created successfully" });
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
